e07cbd05ef12ae840daa187947201113f1a19c69
Tanu.W <cruzercruz@gmail.com>
2020-08-19 11:44:42 +0700

    First upload
    First upload

---
+++ BulkSMSResponse.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestSendSMSByAPI
+{
+
+    public class BulkSMSResponse
+    {
+        public bool Success { get; set; }
+        public string ReasonPhrase { get; set; }
+        public StatusCode StatusCode { get; set; }
+        public BlukSMS BulkSMS { get; set; }
+        public string MessageFromServer { get; set; }
+        //public string AccessToken { get; set; }
+
+        public BulkSMSResponse()
+        {
+            Success = false;
+            ReasonPhrase = "";
+            StatusCode = new StatusCode();
+            BulkSMS = new BlukSMS();
+            MessageFromServer = "";
+            //AccessToken = "";
+        }
+    }
+
+    public class StatusCode
+    {
+        public HttpStatusCode Code { get; set; }
+        public string Text { get; set; }
+
+        public StatusCode()
+        {
+            Code = HttpStatusCode.NotModified;
+            Text = Code.ToString();
+        }
+    }
+
+    public class BlukSMS
+    {
+        public MessageTag Message { get; set; }
+        public string XMLResponse { get; set; }
+
+        public BlukSMS()
+        {
+            Message = new MessageTag();
+            XMLResponse = "";
+        }
+    }
+
+    public class MessageTag
+    {
+        public string ID { get; set; }
+        public Rsr Rsr { get; set; }
+        public Destination Destination { get; set; }
+        public Source Source { get; set; }
+        public RsrDetail RsrDetail { get; set; }
+
+        public MessageTag()
+        {
+            ID = "";
+            Rsr = new Rsr();
+            Destination = new Destination();
+            Source = new Source();
+            RsrDetail = new RsrDetail();
+        }
+    }
+
+    public class Rsr
+    {
+        public string Type { get; set; }
+        public string ServiceID { get; set; }
+
+        public Rsr()
+        {
+            Type = "";
+            ServiceID = "";
+        }
+    }
+
+    public class RsrDetail
+    {
+        public string Status { get; set; }
+        public string Code { get; set; }
+        public string Description { get; set; }
+
+        public RsrDetail()
+        {
+            Status = "";
+            Code = "";
+            Description = "";
+        }
+    }
+
+    public class Destination
+    {
+        public string ID { get; set; }
+        public Address Address { get; set; }
+
+        public Destination()
+        {
+            ID = "";
+            Address = new Address();
+        }
+    }
+
+    public class Source
+    {
+        public Address Address { get; set; }
+
+        public Source()
+        {
+            Address = new Address();
+        }
+    }
+
+    public class Address
+    {
+        public string Number { get; set; }
+
+        public Address()
+        {
+            Number = "";
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace TestSendSMSByAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            SendSMS("66xxxxxxxxx", "Call API with C#", tokenId: "My token id");
+
+
+            Console.ReadKey();
+        }
+
+        public static void SendSMS(string phoneNumber, string message, string tokenId = "")
+        {
+            var httpWebRequest = (HttpWebRequest)WebRequest.Create(@"<YOUR-BULK-SMS-API>");
+            httpWebRequest.ContentType = "application/json; charset=utf-8";
+            httpWebRequest.Method = "POST";
+            httpWebRequest.Accept = "application/json; charset=utf-8";
+
+            var hasToken = "true";// string.IsNullOrEmpty(tokenId.Trim()) ? "false" : "true";
+            using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+            {
+                string loginjson = @"{
+                                        'TokenID':'"+ tokenId + @"',
+                                        'SkipToken':" + hasToken + @",
+                                        'PhoneNumber':'" + phoneNumber + @"',
+                                        'MessageDetail':{
+                                                        'Message':'" + message + @"',
+                                            'LinkMessage':[
+                                                {
+                                                    'Name':'linkToGoogle',
+                                                    'Link':'http://www.google.com',
+                                                    'Display':'Google'
+                                                },
+                                                {
+                                                    'Name':'linkToFacebook',
+                                                    'Link':'http://www.Facebook.com',
+                                                    'Display':'Facebook'
+                                                }
+                                            ]
+                                        },
+                                        'RequestBy':'somebody'
+                                    }";
+
+                streamWriter.Write(loginjson);
+                streamWriter.Flush();
+                streamWriter.Close();
+
+                var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+                using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+                {
+                    var result = streamReader.ReadToEnd();
+                    JsonSerializer serializer = new JsonSerializer();
+                    JToken token = JToken.Parse(result);
+                    JObject json = JObject.Parse((string)token);
+                    BulkSMSResponse bulkSMSResponse = json.ToObject<BulkSMSResponse>();
+
+                    Console.WriteLine(JsonConvert.SerializeObject(bulkSMSResponse));
+
+
+                }
+            }
+        }
+    }
+}
